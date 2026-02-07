fcf656fcea439a9e0ebc20814f82abd5b569b4b4
Bo Maryniuk <bo@maryniuk.net>
2026-02-07 11:49:54 +0100

    Move out unit tests

--- logjetd/src/protocol.rs
+++ logjetd/src/protocol.rs
@@ -119,32 +119,5 @@ pub fn write_replay_request<W: Write>(writer: &mut W, request: &ReplayRequest) -
 }
 
 #[cfg(test)]
-mod tests {
-    use super::{
-        ReplayRequest, WireRecord, read_record, read_replay_request, write_record, write_replay_request,
-    };
-    use logjet::RecordType;
-
-    #[test]
-    fn round_trip_record() {
-        let record = WireRecord {
-            record_type: RecordType::Logs,
-            seq: 42,
-            ts_unix_ns: 77,
-            payload: b"abc".to_vec(),
-        };
-        let mut bytes = Vec::new();
-        write_record(&mut bytes, &record).unwrap();
-        let decoded = read_record(&mut bytes.as_slice()).unwrap().unwrap();
-        assert_eq!(decoded, record);
-    }
-
-    #[test]
-    fn replay_request_round_trip() {
-        let request = ReplayRequest { from_seq: 1234 };
-        let mut bytes = Vec::new();
-        write_replay_request(&mut bytes, &request).unwrap();
-        let decoded = read_replay_request(&mut bytes.as_slice()).unwrap();
-        assert_eq!(decoded, request);
-    }
-}
+#[path = "protocol_utst.rs"]
+mod protocol_utst;
