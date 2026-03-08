127a82f1af0dd6a73aa56a45d289283098c58917
Bo Maryniuk <bo@maryniuk.net>
2026-03-08 16:34:21 +0100

    Add TLS UT

--- logjetd/src/tls_utst.rs
+++ logjetd/src/tls_utst.rs
@@ -1,5 +1,17 @@
-use super::{authority_host, parse_server_name};
-use crate::config::TlsConfig;
+use super::{
+    authority_host, load_client_config, load_ingest_server_config, load_server_config, parse_server_name,
+};
+use crate::config::{IngestTlsConfig, TlsConfig};
+use std::path::PathBuf;
+
+fn demo_cert_path(name: &str) -> PathBuf {
+    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("..")
+        .join("demo")
+        .join("remote-drain-tls")
+        .join("certs")
+        .join(name)
+}
 
 #[test]
 fn authority_host_strips_port() {
@@ -38,4 +50,63 @@ fn parse_server_name_accepts_ip_authority() {
     };
 
     parse_server_name(&tls, "127.0.0.1:7002").unwrap();
+}
+
+#[test]
+fn load_client_config_requires_ca_file() {
+    let tls = TlsConfig {
+        enable: true,
+        ca_file: None,
+        cert_file: None,
+        key_file: None,
+        require_client_cert: false,
+        server_name: None,
+    };
+
+    let err = load_client_config(&tls).unwrap_err();
+    assert_eq!(err.kind(), std::io::ErrorKind::InvalidInput);
+}
+
+#[test]
+fn load_server_config_requires_cert_and_key() {
+    let tls = TlsConfig {
+        enable: true,
+        ca_file: None,
+        cert_file: None,
+        key_file: None,
+        require_client_cert: false,
+        server_name: None,
+    };
+
+    let err = load_server_config(&tls).unwrap_err();
+    assert_eq!(err.kind(), std::io::ErrorKind::InvalidInput);
+}
+
+#[test]
+fn load_server_config_requires_ca_when_client_certs_required() {
+    let tls = TlsConfig {
+        enable: true,
+        ca_file: None,
+        cert_file: Some(demo_cert_path("appliance.pem")),
+        key_file: Some(demo_cert_path("appliance.key")),
+        require_client_cert: true,
+        server_name: None,
+    };
+
+    let err = load_server_config(&tls).unwrap_err();
+    assert_eq!(err.kind(), std::io::ErrorKind::InvalidInput);
+}
+
+#[test]
+fn load_ingest_server_config_requires_cert_and_key() {
+    let tls = IngestTlsConfig {
+        enable: true,
+        ca_file: None,
+        cert_file: None,
+        key_file: None,
+        require_client_cert: false,
+    };
+
+    let err = load_ingest_server_config(&tls).unwrap_err();
+    assert_eq!(err.kind(), std::io::ErrorKind::InvalidInput);
 }
